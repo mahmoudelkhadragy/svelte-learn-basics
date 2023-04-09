1f346563036aed06b84a78d2c44fc2f09150f8a0
mahmoudelkhadragy <mahmoudelkhadragy38@gmail.com>
2023-04-09 03:51:49 +0200

    add all project

---
+++ src/App.svelte
@@ -0,0 +1,132 @@
+<script>
+  import MeetupGrid from "./Meetups/MeetupGrid.svelte";
+  import Header from "./UI/Header.svelte";
+  import TextInput from "./UI/TextInput.svelte";
+
+  let title = '';
+  let subtitle = '';
+  let description = '';
+  let imageUrl = '';
+  let address = '';
+  let email = '';
+
+  let meetups = [
+    {
+    id: 1,
+    title: 'React Meetup',
+    subtitle: 'A meetup for React enthusiasts',
+    description: 'Join us to learn more about React and its ecosystem',
+    imageUrl: 'https://via.placeholder.com/1000x450',
+    address: '123 Main St, Anytown USA',
+    contactEmail: 'reactmeetup@example.com'
+  },
+  {
+    id: 2,
+    title: 'Angular Meetup',
+    subtitle: 'A meetup for Angular enthusiasts',
+    description: 'Join us to learn more about Angular and its ecosystem',
+    imageUrl: 'https://via.placeholder.com/1000x450',
+    address: '456 Oak St, Anytown USA',
+    contactEmail: 'angularmeetup@example.com'
+  },
+  {
+    id: 3,
+    title: 'Vue Meetup',
+    subtitle: 'A meetup for Vue enthusiasts',
+    description: 'Join us to learn more about Vue and its ecosystem',
+    imageUrl: 'https://via.placeholder.com/1000x450',
+    address: '789 Elm St, Anytown USA',
+    contactEmail: 'vuemeetup@example.com'
+  },
+  {
+    id: 4,
+    title: 'Node.js Meetup',
+    subtitle: 'A meetup for Node.js enthusiasts',
+    description: 'Join us to learn more about Node.js and its ecosystem',
+    imageUrl: 'https://via.placeholder.com/1000x450',
+    address: '321 Pine St, Anytown USA',
+    contactEmail: 'nodejsmeetup@example.com'
+  },
+  {
+    id: 5,
+    title: 'Python Meetup',
+    subtitle: 'A meetup for Python enthusiasts',
+    description: 'Join us to learn more about Python and its ecosystem',
+    imageUrl: 'https://via.placeholder.com/1000x450',
+    address: '654 Cedar St, Anytown USA',
+    contactEmail: 'pythonmeetup@example.com'
+  }
+];
+
+function addMeetup(){
+  const newMeetup = {
+    id: Math.random().toString(),
+    contactEmail: email,
+    title,
+    subtitle,
+    description,
+    imageUrl,
+    address
+  }
+
+  meetups = [newMeetup, ...meetups]
+}
+
+</script>
+<style>
+  main{
+    margin-top: 5rem;
+  }
+  form{
+    width: 30rem;
+    max-width: 90%;
+    margin: auto;
+  }
+</style>
+<Header />
+
+<main>
+  <form on:submit|preventDefault={addMeetup}>
+    <TextInput
+      label="Title" 
+      id="title"
+      value={title}
+      on:input={event => title = event.target.value}
+    />
+    <TextInput
+      label="Subtitle" 
+      id="subtitle"
+      value={subtitle}
+      on:input={event => subtitle = event.target.value}
+    />
+    <TextInput
+      label="ImageUrl" 
+      id="imageUrl"
+      value={imageUrl}
+      on:input={event => imageUrl = event.target.value}
+    />
+    <TextInput
+      label="Address" 
+      id="address"
+      value={address}
+      on:input={event => address = event.target.value}
+    />
+    <TextInput
+      label="Email" 
+      id="email"
+      value={email}
+      controlType ='email'
+      on:input={event => email = event.target.value}
+    />
+    <TextInput
+      label="description" 
+      id="description"
+      value={description}
+      controlType ='textarea'
+      on:input={event => description = event.target.value}
+    />
+    <button>Add Meetup</button>
+  </form>
+</main>
+
+<MeetupGrid {meetups} />
---
+++ src/Meetups/MeetupItem.svelte
@@ -0,0 +1,81 @@
+
+
+<script>
+  export let title;
+  export let subtitle;
+  export let description;
+  export let imageUrl;
+  export let address;
+  export let contactEmail;
+
+</script>
+<style>
+  article {
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+    border-radius: 5px;
+    background: white;
+    margin: 1rem;
+  }
+
+  header,
+  .content,
+  footer {
+    padding: 1rem;
+  }
+
+  .image {
+    width: 100%;
+    height: 14rem;
+  }
+
+  .image img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+
+  h1 {
+    font-size: 1.25rem;
+    margin: 0.5rem 0;
+    font-family: "Roboto Slab", sans-serif;
+  }
+
+  h1.is-favorite {
+    background: #01a129;
+    color: white;
+    padding: 0 0.5rem;
+    border-radius: 5px;
+  }
+
+  h2 {
+    font-size: 1rem;
+    color: #808080;
+    margin: 0.5rem 0;
+  }
+
+  p {
+    font-size: 1.25rem;
+    margin: 0;
+  }
+</style>
+
+
+
+<article>
+  <header>
+    <h1>{title}</h1>
+    <h2>{subtitle}</h2>
+  </header>
+  <div class="image">
+    <img src="{imageUrl}" alt="{title}" />
+  </div>
+  <div class="content">
+    <p><b>Description:</b> {description}</p>
+    <p><b>Address:</b> {address}</p>
+    <p><b>Email:</b> <a href="mailto:{contactEmail}">Contact Email</a></p>
+  </div>
+  <footer>
+    <button>Show Details</button>
+    <button class="outline">Favorite</button>
+  </footer>
+</article>
